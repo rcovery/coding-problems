6c2d8bd7804bd3480f157353603aa0dc32af4102
rcovery <ryanczpereira@gmail.com>
2022-01-16 23:11:51 -0300

    Toeplitz Matrix

---
+++ 1_ToeplitzMatrix/problem.py
@@ -0,0 +1,14 @@
+array = [
+	[3,5,7],
+	[9,3,5],
+	[4,9,3]
+]
+
+def isToeplitz(arr):
+	for fooIndex, foo in enumerate(arr):
+		for barIndex, bar in enumerate(foo):
+			if fooIndex > 0 and barIndex > 0:
+				if (array[fooIndex - 1][barIndex - 1] == bar):
+					return True;
+
+print(isToeplitz(array))
